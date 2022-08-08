6d31b2539e7883a56b0f3ad7eb019d02cfe1c216
Andrew <cosminfuica@gmail.com>
2022-08-08 16:25:29 +0300

    configure deployment on AWS Cloud
    link to swagger :  http://springbootblogapp-env.eba-k34dcz2t.eu-west-2.elasticbeanstalk.com/swagger-ui/index.html#/

--- JavaBlogApp/springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ JavaBlogApp/springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,6 +1,10 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
     //Folosim ModelMapper pentru a mapa automat un obiect in alt obiect
     @Bean
@@ -16,8 +20,22 @@ public class SpringbootBlogRestApiApplication {
         return new ModelMapper();
     }
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
     }
 
+    //Cream 2 tipuri de utilizatori in baza de date
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
+    }
 }
